62b32f186c0b8df94417bad5c76f48b9331655fe
Giova262 <jvaldiviau@fi.uba.ar>
2023-04-01 21:56:25 -0300

    agrego rutas protegidas

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -14,13 +14,14 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $validateData = $request->validate([
-            'name' => '',
-            'email' => '',
-            'password' => '',
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
         ]);
 
         $validateData['password'] = Hash::make($request->password);
         $user = User::create($validateData);
+
         $accessToken = $user->createToken('authToken')->accessToken;
 
         return response([
@@ -32,17 +33,13 @@ class AuthController extends Controller
     public function login(Request $request )
     {
         $loginData = $request->validate([
-            'name' => '',
-            'email' => '',
-            'password' => '',
+            'name' => 'required',
+            'password' => 'required',
         ]);
 
-        //return (array)auth() ;
-
         if (!auth()->attempt($loginData)) {
-            return response(['message' => 'credenciales invaliudas'], 422);
+            return response(['message' => 'credenciales invaliudas']);
         }
-
 
         $accessToken = auth()->user()->createToken('authToken')->accessToken;
 
@@ -50,6 +47,5 @@ class AuthController extends Controller
             'user' => auth()->user(),
             'accessToken' => $accessToken,
         ]);
-
     }
 }
